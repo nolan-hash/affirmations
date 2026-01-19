09b0731b087f2ec3f9379b3eaf95f449dc712eb8
nolan-hash <nolan.perry@sonomaacademy.org>
2026-01-18 23:46:38 -0800

    Update affirm.js

--- api/affirm.js
+++ api/affirm.js
@@ -1,3 +1,17 @@
 export default function handler(req, res) {
-  res.status(200).json({ affirm: "test works" });
+  const strings = [
+    "You are so beautiful",
+    "I love youuuu",
+    "I miss you",
+    "Call me",
+    "I want to give you a hug",
+    "You are a great person"
+  ];
+
+  const randomIndex = Math.floor(Math.random() * strings.length);
+  const randomString = strings[randomIndex] + " :)";
+
+  res.status(200).json({
+    randomString
+  });
 }
